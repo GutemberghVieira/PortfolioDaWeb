05faaf0e8a358c4f6042ed862296283fe8447ced
GutemberghVieira <gutembergh203@gmail.com>
2022-03-10 22:40:54 -0300

    ok

--- portfólio.js
+++ portfólio.js
@@ -1,3 +1,7 @@
  document.querySelector(".Grid").addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
 });
+
+document.getElementById("Groove").addEventListener("click", function(){
+    alert("Muito Obrigado por me enviar sua Mensagem");
+})
